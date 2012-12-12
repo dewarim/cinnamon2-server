e972219605bb213bd47b952bcd678e58061ecbdc
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-12-12 18:20:48 +0100

    add checkout.sh to clone all git repositories for Cinnamon server v2

---
+++ checkout.sh
@@ -0,0 +1,31 @@
+# clone all git repositories of Cinnamon Server v2 from Github.
+  
+# Renderserver: fetch tasks from a repository, execute them and upload the results.
+git clone git@github.com:dewarim/cinnamon2-renderserver.git
+
+# CinnamonBase: essential classes required by probably all Cinnamon code (interfaces, constants, exceptions)
+git clone git@github.com:dewarim/cinnamon2-base.git
+
+# CinnamonUtils: Utility classes (encryption, configuration handling, interfaces) 
+git clone git@github.com:dewarim/cinnamon2-utils.git
+
+# CinnamonEntitylib: Domain classes with JPA / Hibernate Annotations and Lucene indexing 
+git clone git@github.com:dewarim/cinnamon2-entitylib.git
+
+# CinnamonServer: the main server code
+git clone git@github.com:dewarim/cinnamon2-server.git
+
+# Dandelion: administrative Grails app for Cinnamon server.
+git clone git@github.com:dewarim/cinnamon2-dandelion.git
+
+# Illicium: Simple webclient in Grails for Cinnamon server.
+git clone git@github.com:dewarim/cinnamon2-illicium.git
+
+# Humulus: base plugin class for Grails based Cinnamon 2 projects (like Dandelion and Illicium) 
+git clone git@github.com:dewarim/cinnamon2-humulus.git
+
+# Cinnamon2-clientlib: Java client library for Cinnamon server.
+git clone git@github.com:dewarim/cinnamon2-clientlib.git
+
+# cinnamon-demo-data: DITA demo dataset and configuration files for Cinnamon server.
+git clone git@github.com:dewarim/cinnamon-demo-data.git
