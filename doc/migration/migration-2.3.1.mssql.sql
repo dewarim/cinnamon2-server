5356d983a33413be74f80cd4d55e3620c49fe26e
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-01-08 16:53:22 +0100

    Added sql migration script for MS-SQL server 2008 from 2.3.0 to current version.

---
+++ doc/migration/migration-2.3.1.mssql.sql
@@ -0,0 +1,62 @@
+USE [demo]
+GO
+
+/****** Object:  Table [dbo].[links]    Script Date: 01/08/2013 06:40:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[links](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[type] [nvarchar](255) NOT NULL,
+	[resolver] [nvarchar](255) NOT NULL,
+	[owner_id] [bigint] NOT NULL,
+	[parent_id] [bigint] NOT NULL,
+	[acl_id] [bigint] NOT NULL,
+	[folder_id] [bigint] NULL,
+	[osd_id] [bigint] NULL,
+ CONSTRAINT [PK_links] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[links]  WITH CHECK ADD  CONSTRAINT [FK_links_acls] FOREIGN KEY([acl_id])
+REFERENCES [dbo].[acls] ([id])
+GO
+
+ALTER TABLE [dbo].[links] CHECK CONSTRAINT [FK_links_acls]
+GO
+
+ALTER TABLE [dbo].[links]  WITH CHECK ADD  CONSTRAINT [FK_links_folder_parent] FOREIGN KEY([parent_id])
+REFERENCES [dbo].[folders] ([id])
+GO
+
+ALTER TABLE [dbo].[links] CHECK CONSTRAINT [FK_links_folder_parent]
+GO
+
+ALTER TABLE [dbo].[links]  WITH CHECK ADD  CONSTRAINT [FK_links_folders] FOREIGN KEY([folder_id])
+REFERENCES [dbo].[folders] ([id])
+GO
+
+ALTER TABLE [dbo].[links] CHECK CONSTRAINT [FK_links_folders]
+GO
+
+ALTER TABLE [dbo].[links]  WITH CHECK ADD  CONSTRAINT [FK_links_objects] FOREIGN KEY([osd_id])
+REFERENCES [dbo].[objects] ([id])
+GO
+
+ALTER TABLE [dbo].[links] CHECK CONSTRAINT [FK_links_objects]
+GO
+
+ALTER TABLE [dbo].[links]  WITH CHECK ADD  CONSTRAINT [FK_links_users] FOREIGN KEY([owner_id])
+REFERENCES [dbo].[users] ([id])
+GO
+
+ALTER TABLE [dbo].[links] CHECK CONSTRAINT [FK_links_users]
+GO
+
