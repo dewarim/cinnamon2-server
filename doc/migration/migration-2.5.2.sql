859223afebbad88363a796b88f0375e4169b18d7
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-05-16 17:19:12 +0200

    Fixed migration script.

--- doc/migration/migration-2.5.2.sql
+++ doc/migration/migration-2.5.2.sql
@@ -1,2 +1,2 @@
 alter table relationtypes add column clone_on_left_version boolean not null default false;
-alter table relationtypes add column clone_on_left_version boolean not null default false;
+alter table relationtypes add column clone_on_right_version boolean not null default false;
