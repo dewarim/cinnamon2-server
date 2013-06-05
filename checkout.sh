213373af12f7d0c7bb881434abf0a50f5842b48e
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-06-05 14:12:39 +0200

    fixed checkout script

--- checkout.sh
+++ checkout.sh
@@ -1,37 +1,37 @@
 echo "Clone all git repositories of Cinnamon Server v2 from Github."
   
 # Renderserver: fetch tasks from a repository, execute them and upload the results.
-git clone git@github.com:dewarim/cinnamon2-renderserver.git
+git clone git://github.com/dewarim/cinnamon2-renderserver.git
 
 # CinnamonBase: essential classes required by probably all Cinnamon code (interfaces, constants, exceptions)
-git clone git@github.com:dewarim/cinnamon2-base.git
+git clone git://github.com/dewarim/cinnamon2-base.git
 
 # CinnamonUtils: Utility classes (encryption, configuration handling, interfaces) 
-git clone git@github.com:dewarim/cinnamon2-utils.git
+git clone git://github.com/dewarim/cinnamon2-utils.git
 
 # CinnamonEntitylib: Domain classes with JPA / Hibernate Annotations and Lucene indexing 
-git clone git@github.com:dewarim/cinnamon2-entitylib.git
+git clone git://github.com/dewarim/cinnamon2-entitylib.git
 
 # CinnamonServer: the main server code
-git clone git@github.com:dewarim/cinnamon2-server.git
+git clone git://github.com/dewarim/cinnamon2-server.git
 
 # Dandelion: administrative Grails app for Cinnamon server.
-git clone git@github.com:dewarim/cinnamon2-dandelion.git
+git clone git://github.com/dewarim/cinnamon2-dandelion.git
 
 # Illicium: Simple webclient in Grails for Cinnamon server.
-git clone git@github.com:dewarim/cinnamon2-illicium.git
+git clone git://github.com/dewarim/cinnamon2-illicium.git
 
 # Humulus: base plugin class for Grails based Cinnamon 2 projects (like Dandelion and Illicium) 
-git clone git@github.com:dewarim/cinnamon2-humulus.git
+git clone git://github.com/dewarim/cinnamon2-humulus.git
 
 # Cinnamon2-clientlib: Java client library for Cinnamon server.
-git clone git@github.com:dewarim/cinnamon2-clientlib.git
+git clone git://github.com/dewarim/cinnamon2-clientlib.git
 
 # cinnamon-demo-data: DITA demo dataset and configuration files for Cinnamon server.
-git clone git@github.com:dewarim/cinnamon2-demo-data.git
+git clone git://github.com/dewarim/cinnamon2-demo-data.git
 
 # Cinnamon2-Tools: Repository cleanup and conversion tools for Cinnamon.
-git clone git@github.com:dewarim/cinnamon2-tools.git
+git clone git://github.com/dewarim/cinnamon2-tools.git
 
 echo "Finished cloning git repositories."
 echo "To download the depenencies, use: 'wget http://download.horner-code.de/cinnamon/cinnamon-dependencies-2.4.1.zip'"
